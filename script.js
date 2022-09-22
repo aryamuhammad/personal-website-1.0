ea6b3a5b0aa8675c783a053c20d4cbffa48ea7f2
Muhammad Arya Wirawan <maryawirawan13@gmail.com>
2022-09-22 15:06:08 +0700

    add smooth scrolling and modal submitted message

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ document.documentElement.style.setProperty(
   "--scroll-padding",
   navigationHeight + "px"
 );
+
+//form
+const nameInput = document.querySelector(".name");
+const emailInput = document.querySelector(".email");
+const messageInput = document.querySelector(".message");
+const buttonSubmit = document.querySelector(".submit");
+const modalCard = document.querySelector(".modal");
+const buttonModal = document.getElementById("submit");
+const messageModal = document.querySelector(".modal .body-modal h1");
+buttonSubmit.addEventListener("click", function () {
+  messageModal.innerHTML = `Thank you ${nameInput.value}!`;
+  nameInput.value = "";
+  emailInput.value = "";
+  messageInput.value = "";
+  modalCard.style.display = "block";
+});
+
+buttonModal.addEventListener("click", function () {
+  modalCard.style.display = "none";
+});
